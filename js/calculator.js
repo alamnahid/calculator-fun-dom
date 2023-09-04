5099eb87dd80c50d4aed7dca2cf705ade8997e1a
alamnahid <nahidalam13765@gmail.com>
2023-09-04 21:54:47 +0600

    first commit

---
+++ js/calculator.js
@@ -0,0 +1,197 @@
+const result = document.getElementById('result');
+
+document.getElementById('seven').addEventListener('click', function(){
+    const sevenField = document.getElementById('seven');
+    const sevenString = sevenField.innerText;
+    const seven = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = seven;
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
+document.getElementById('eight').addEventListener('click', function(){
+    const sevenField = document.getElementById('eight');
+    const sevenString = sevenField.innerText;
+    const eight = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = eight;
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
+document.getElementById('nine').addEventListener('click', function(){
+    const sevenField = document.getElementById('nine');
+    const sevenString = sevenField.innerText;
+    const nine = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = nine;
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
+document.getElementById('four').addEventListener('click', function(){
+    const sevenField = document.getElementById('four');
+    const sevenString = sevenField.innerText;
+    const four = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = four;
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
+document.getElementById('five').addEventListener('click', function(){
+    const sevenField = document.getElementById('five');
+    const sevenString = sevenField.innerText;
+    const five = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = five;
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
+document.getElementById('six').addEventListener('click', function(){
+    const sevenField = document.getElementById('six');
+    const sevenString = sevenField.innerText;
+    const six = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = six;
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
+document.getElementById('one').addEventListener('click', function(){
+    const sevenField = document.getElementById('one');
+    const sevenString = sevenField.innerText;
+    const one = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = one;
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
+document.getElementById('two').addEventListener('click', function(){
+    const sevenField = document.getElementById('two');
+    const sevenString = sevenField.innerText;
+    const two = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = two;
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
+document.getElementById('three').addEventListener('click', function(){
+    const sevenField = document.getElementById('three');
+    const sevenString = sevenField.innerText;
+    const three = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = three;
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
+document.getElementById('zero').addEventListener('click', function(){
+    const sevenField = document.getElementById('zero');
+    const sevenString = sevenField.innerText;
+    const zero = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = zero;
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
+document.getElementById('dot').addEventListener('click', function(){
+    const sevenField = document.getElementById('dot');
+    const sevenString = sevenField.innerText;
+    const dot = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = '.';
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
+document.getElementById('doublezero').addEventListener('click', function(){
+    const sevenField = document.getElementById('doublezero');
+    const sevenString = sevenField.innerText;
+    const doublezero = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = '00';
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
+document.getElementById('divide').addEventListener('click', function(){
+    const sevenField = document.getElementById('divide');
+    const sevenString = sevenField.innerText;
+    const divide = parseFloat(sevenString);
+
+    const p = document.createElement('p');
+    p.innerText = '/';
+    p.classList.add('bg-[#E2FCFF]');
+    p.classList.add('text-5xl');
+    p.classList.add('text-right');
+    p.classList.add('mt-4');
+    p.classList.add('font-semibold');
+    p.classList.add('mr-4');
+    result.appendChild(p)
+})
